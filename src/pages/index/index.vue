9723354061ca12d4667807d9dd645a961477c1dc
sjy1158 <1124080755@qq.com>
2019-03-03 17:33:50 +0800

    sjy

---
+++ src/pages/index/index.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <!--banner-->
+    <div>
+      <swiper :indicator-dots="swiperParams.indicatorDots"
+              :autoplay="swiperParams.autoplay"
+              :interval="swiperParams.interval"
+              indicator-color="rgba(255, 255, 255, .5)"
+              indicator-active-color="rgba(255, 255, 255, 1)"
+              :duration="swiperParams.duration"
+              :circular="swiperParams.circular" style="height: 220px;">
+        <div>
+          <swiper-item>
+            <image lazy-load="true" src="/static/home/banner.png" class="slide-image">
+            </image>
+          </swiper-item>
+        </div>
+      </swiper>
+    </div>
+
+    <div class="contentlist" style="height:auto;width: 100%;background: white;border-radius: 10px;margin-top: -20px;position: absolute">
+      <div class="itemtype">
+        <div>
+          <img src="/static/home/item1.png" alt="">
+          <p>微活动</p>
+        </div>
+        <div>
+          <img src="/static/home/item2.png" alt="">
+          <p>微传单</p>
+        </div>
+        <div>
+          <img src="/static/home/item3.png" alt="">
+          <p>邀请函</p>
+        </div>
+      </div>
+
+      <swiper :indicator-dots="swiperParams.indicatorDots"
+              :autoplay="swiperParams.autoplay"
+              :interval="swiperParams.interval"
+              indicator-color="rgba(255, 255, 255, .5)"
+              indicator-active-color="rgba(255, 255, 255, 1)"
+              :duration="swiperParams.duration"
+              :circular="swiperParams.circular" style="height: 64px;width: 100%;margin-top: 17px">
+            <swiper-item>
+              <div style="padding-left: 15px;padding-right: 15px;">
+                <div style="width: 100%;height: 64px;">
+                  <image lazy-load="true" src="/static/home/banner2.png" class="slide-image">
+                  </image>
+                </div>
+              </div>
+            </swiper-item>
+            <swiper-item>
+              <div style="padding-left: 15px;padding-right: 15px;">
+                <div style="width: 100%;height: 64px;">
+                  <image lazy-load="true" src="/static/home/banner2.png" class="slide-image">
+                  </image>
+                </div>
+              </div>
+            </swiper-item>
+      </swiper>
+
+      <!--精选模版-->
+      <div style="width: 100%;height: 48px;line-height: 48px;padding-left: 15px;color: #000000;font-size: 14px;font-weight: bold;letter-spacing: 1px;">精选模板</div>
+      <div class="">
+        <ul>
+          <li>
+            <img src="/static/home/banner.png" alt="" class="bg">
+            <p>999人制作</p>
+            <img src="/static/home/zhizuo.png" alt="" class="zhizuo">
+          </li>
+          <li>
+            <img src="/static/home/banner.png" alt="" class="bg">
+            <p>999人制作</p>
+            <img src="/static/home/zhizuo.png" alt="" class="zhizuo">
+          </li>
+          <li>
+            <img src="/static/home/banner.png" alt="" class="bg">
+            <p>999人制作</p>
+            <img src="/static/home/zhizuo.png" alt="" class="zhizuo">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      swiperParams:　{
+        indicatorDots: true,
+        autoplay: true,
+        interval: 1000,
+        duration: 900,
+        circular: true
+      }
+    }
+  },
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+  .slide-image{
+    height: 100%;
+    width: 100%;
+  }
+  .itemtype{
+    margin-top: 17px;
+    display: flex;
+  }
+  .itemtype div{
+    flex: 1;
+    text-align: center;
+    font-size: 14px;
+    color: #000000;
+    letter-spacing: 2px;
+  }
+  .itemtype div p{
+    padding-top: 8px;
+  }
+  .itemtype div img{
+    height: 44px;
+    width: 44px;
+  }
+  ul{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  ul li {
+    width: 33.3%;
+    height: 184px;
+    position: relative;
+    /*background: red;*/
+    color: #000000;
+    font-size: 12px;
+    letter-spacing: 1px;
+  }
+  ul li p {
+    position: absolute;
+    /*left: 0px;*/
+    bottom: 11px;
+    left: 7%;
+  }
+  ul li img.bg{
+    height: 150px;
+    width: 86%;
+    position: absolute;
+    left: 50%;
+    margin-left: -43%;
+  }
+  ul li img.zhizuo{
+    position: absolute;
+    height: 34px;
+    width: 61px;
+    right: 0px!important;
+    bottom: 0px;
+  }
+</style>
---
+++ src/pages/logs/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <div class="headerbg">
+      <img src="/static/home/banner.png" alt="">
+    </div>
+
+    <div style="position: relative;width: 100%;z-index: 9999">
+      <div class="title">
+        <div class="headertext"></div>
+      </div>
+    </div>
+
+    <div class="time" style="position: relative">
+      <div class="timecontent">
+        <div class="alltime">
+          <img src="/static/home/item1.png" alt="">
+          <span class="huiyuan">月卡会员</span>
+          <span style="float: right;margin-right: 40px;" class="daoqi">2019年2月20日到期</span>
+        </div>
+      </div>
+    </div>
+
+    <div style="width: 100%;height: 46px;line-height: 46px;letter-spacing: 2px;font-size: 14px;color: #000000;padding-left: 30px;">商家服务</div>
+
+    <div class="items">
+      <ul>
+        <li>
+          <img src="/static/my/my1.png" alt="">
+          <p>我的活动</p>
+        </li>
+        <li>
+          <img src="/static/my/my2.png" alt="">
+          <p>我的传单</p>
+        </li>
+        <li>
+          <img src="/static/my/my3.png" alt="">
+          <p>邀请函</p>
+        </li>
+        <li>
+          <img src="/static/my/my4.png" alt="">
+          <p>帮助中心</p>
+        </li>
+        <li>
+          <img src="/static/my/my5.png" alt="">
+          <p>我的客服</p>
+        </li>
+        <li>
+          <img src="/static/my/my6.png" alt="">
+          <p>安全退出</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style>
+  .headerbg{
+    width: 100%;
+    height: 150px;
+  }
+  .headerbg img{
+    height: 100%;
+    width: 100%;
+  }
+  .title{
+    padding-left: 15px;
+    padding-right: 15px;
+    height: 120px;
+  }
+  .headertext{
+    width: 100%;
+    height: 120px;
+    background:white;
+    margin-top: -60px;
+    border-radius: 10px;
+  }
+  .time {
+    padding-left: 15px;
+    padding-right: 15px;
+    height: 80px;
+  }
+  .timecontent{
+    width: 100%;
+    height: 80px;
+    background: #9FE6FF;
+    margin-top: -40px;
+    border-radius: 10px;
+  }
+  .alltime{
+    width: 100%;
+    height: 40px;
+    position: absolute;
+    bottom: 0px;
+    line-height: 40px;
+    letter-spacing: 2px;
+  }
+  .alltime span{
+    vertical-align: middle;
+  }
+  .alltime .huiyuan{
+    margin-left: 15px;
+    font-size: 14px;
+    color: #1179FF;
+  }
+  .alltime .daoqi{
+    color: #000000;
+    font-size: 12px;
+  }
+  .alltime img{
+    width: 20px;
+    height: 20px;
+    padding-left: 15px;
+    vertical-align: middle;
+  }
+
+  ul{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  ul li {
+    width: 25%;
+    text-align: center;
+    /*background: red;*/
+    color: #000000;
+    font-size: 12px;
+    letter-spacing: 1px;
+  }
+  ul li img {
+    height: 21px;
+    width: 21px;
+  }
+  ul li p{
+    padding-top: 8px;
+    padding-bottom: 20px;
+    letter-spacing: 2px;
+  }
+</style>
